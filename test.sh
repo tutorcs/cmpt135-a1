3fda4f1f993120bdeb9e6091cac25d46e87e1b54
tutorcs <tutorcs@163.com>
2023-02-08 16:42:35 +0800

    WeChat cstutorcs cmpt135 a1

---
+++ test.sh
@@ -0,0 +1,23 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+#!/usr/bin/bash
+
+./myecho Once upon a time.
+./myecho -s Once upon a time.
+./myecho -r Once upon a time.
+./myecho -sr a b c d
+./myecho -rs a b c d
+
+./myecho -q Once upon a time.
+./myecho -qs Once upon a time.
+./myecho -rq Once upon a time.
+./myecho -sqr a b c d
+./myecho -qrs a b c d
+./myecho -q
+./myecho
+
+./myecho -h
+./myecho -qhs one two three
+./myecho -runtests
